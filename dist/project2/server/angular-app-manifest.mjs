7581f7f43b1a5e006ceec0ecde3487bbea112bd4
viswamac <viswa.vbd99@gmail.com>
2025-04-06 12:11:59 +0530

    changes2

---
+++ dist/project2/server/angular-app-manifest.mjs
@@ -0,0 +1,14 @@
+
+export default {
+  bootstrap: () => import('./main.server.mjs').then(m => m.default),
+  inlineCriticalCss: true,
+  baseHref: '/',
+  locale: undefined,
+  routes: undefined,
+  entryPointToBrowserMapping: {},
+  assets: {
+    'index.csr.html': {size: 492, hash: '0933c7c304c21a051d1d0d34042ee90394e410c7d98d51090bb46fa94380eb70', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1005, hash: 'eb471c5b6af76acec9bc202be1190ea27ffc31fe6a6d75f49e711a426f2c9e4e', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
+  },
+};
